185fc73835ae4ac194d0441c907cee0e7d526521
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-14 20:55:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,7 +66,7 @@ def runner
   card_total = initial_round
 
   until 21 < card_total
-    hit?(card_total)
+    card_total = hit?(card_total)
     display_card_total(card_total)
 end
   end_game(card_total)
